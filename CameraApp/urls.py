e5b009529211ca9dfba715dc86353db791500bdb
dhanushkomari <dhanushkomari@gmail.com>
2021-04-01 15:26:14 +0530

    New notebook is added

--- CameraApp/urls.py
+++ CameraApp/urls.py
@@ -4,12 +4,13 @@ from .import views
 app_name = 'CameraApp'
 
 urlpatterns = [
+    ########################   APP URLS   ############################
     path('', views.Index ,name ='index'),
     path('create', views.UserCreateView, name = 'create'),
     path('video_feed', views.video_feed, name = 'video_feed'),
 
-    ##############   API URLS   ##############
+    ########################   API URLS   ############################
     path('api/patients', views.recentPatient, name = 'recent_patient'),
     path('api/status', views.StatusRest, name = 'status'),
-    path('api/all-patients', views.allPatients, name = 'all_patients')
+    path('api/all-patients', views.allPatients, name = 'all_patients'),
 ]
